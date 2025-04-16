0496eadf26f9e09a8394d7a6801b750e766a0fd1
muo <71619031+rapf10@users.noreply.github.com>
2025-04-16 09:28:23 +0700

    Add files via upload

---
+++ Pre-Praktikum/Pre-2.sql
@@ -0,0 +1,14 @@
+-- Nama : Rasya Aditya Pasha Firdaus
+-- Nim : 23241108
+-- Kelas : C
+
+-- Melihat database yang ada
+SHOW Databasis;
+-- Melihat database
+USE mysql;
+-- Melihat tabel dalama database mysql
+SHOW TABLES;
+-- Mendeskribsikan sebuah table yang dipilih ex. table "user"
+DESCRIBE user;
+-- Melihat isi tabel user, khusus untuk kolom Host,User, dan Password
+SELECT Host, User, Password FROM user;
